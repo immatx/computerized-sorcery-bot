bd3e98d352140ba8a8790e30e92b75d45cdb8b00
immatx <immatx@gmail.com>
2017-07-15 01:03:26 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = 'I am Supreme Overlord Samantha Sprecace';
 
   options = {
     hostname: 'api.groupme.com',
